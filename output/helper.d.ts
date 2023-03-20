a352a090abe32d22b6d476f974ce3e12fba8e078
Abolaji Bisiriyu <abolajibisiriyu@gmail.com>
2023-03-20 18:24:00 +0000

    generated develop output

---
+++ output/helper.d.ts
@@ -0,0 +1,21 @@
+import { BushaCommercePayload } from "./types";
+export declare function injectGlobalStyles(): void;
+export declare function validatePayload(p: BushaCommercePayload): {
+    reference?: string | undefined;
+    callback_url?: string | undefined;
+    mode?: string | undefined;
+    meta: {
+        email?: string | undefined;
+        name?: string | undefined;
+    };
+    local_amount: number;
+    local_currency: string;
+    business_id: string;
+};
+export declare function createContainerEl(): HTMLDivElement;
+export declare function createCloseBtnEl(): HTMLButtonElement;
+export declare function createSpinnerEl(): HTMLDivElement;
+export declare function createIframeEl(): HTMLIFrameElement;
+type FormPayload = Omit<BushaCommercePayload, "onClose" | "onSuccess">;
+export declare function createFormEl(payload: FormPayload): HTMLFormElement;
+export {};
