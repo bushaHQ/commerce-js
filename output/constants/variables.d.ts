9e237df5c2917f4b86dd96513263f0bcddee46c1
Abolaji Bisiriyu <abolajibisiriyu@gmail.com>
2023-03-24 14:41:33 +0000

    generated develop output

---
+++ output/constants/variables.d.ts
@@ -0,0 +1,10 @@
+export declare const CONTAINER_ID = "busha-commerce-container";
+export declare const LOADER_ID = "busha-commerce-loader";
+export declare const STYLESHEET_ID = "busha-commerce-styles";
+export declare const CLOSE_BUTTON_ID = "busha-commerce-close-btn";
+export declare const IFRAME_ID = "busha-commerce-iframe";
+export declare const FORM_ID = "busha-commerce-form";
+export declare const PAY_UI: string | undefined;
+export declare const INITIALIZED_STATUS = "INITIALIZED";
+export declare const CANCELLED_STATUS = "CANCELLED";
+export declare const COMPLETED_STATUS = "COMPLETED";
